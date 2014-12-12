646682b056a253f360b94a1ec9f23a94aef9eee2
Rob Winch <rwinch@gopivotal.com>
2014-12-11 20:56:24 -0600

    EnableWebMvcSecurity->EnableWebSecurity

--- samples/websocket/src/main/java/sample/config/WebSecurityConfig.java
+++ samples/websocket/src/main/java/sample/config/WebSecurityConfig.java
@@ -21,15 +21,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableRedisHttpSession
 public class WebSecurityConfig
