5aca5832fcfc59401974210761d631724508221c
Roy Kachouh <rkachouh@squarespace.com>
2015-01-14 17:30:43 -0500

    Remove unnecessary JedisConnectionFactory bean

--- samples/boot/src/main/java/sample/config/HttpSessionConfig.java
+++ samples/boot/src/main/java/sample/config/HttpSessionConfig.java
@@ -1,16 +1,11 @@
 package sample.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession // <1>
 public class HttpSessionConfig {
 
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory(); // <2>
-    }
+
 }
