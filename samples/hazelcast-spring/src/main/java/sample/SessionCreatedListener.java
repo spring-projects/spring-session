e3414ef11eaef6446553c8f9b0ca5e86b94c1f0b
Mark Anderson <manderson23@gmail.com>
2015-08-18 23:38:32 +0100

    Add hazelcast-spring Sample
    Fixes gh-274

---
+++ samples/hazelcast-spring/src/main/java/sample/SessionCreatedListener.java
@@ -0,0 +1,24 @@
+
+package sample;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.events.SessionCreatedEvent;
+
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.map.listener.EntryAddedListener;
+
+public class SessionCreatedListener
+		implements EntryAddedListener<String, ExpiringSession> {
+
+	private ApplicationEventPublisher eventPublisher;
+
+	public SessionCreatedListener(ApplicationEventPublisher eventPublisher) {
+		this.eventPublisher = eventPublisher;
+	}
+
+	public void entryAdded(EntryEvent<String, ExpiringSession> event) {
+		System.out.println("Session added: " + event);
+		eventPublisher.publishEvent(new SessionCreatedEvent(this, event.getValue()));
+	}
+}
---
+++ samples/hazelcast-spring/src/main/java/sample/SessionEvictedListener.java
@@ -0,0 +1,25 @@
+
+package sample;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.events.SessionExpiredEvent;
+
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.map.listener.EntryEvictedListener;
+
+public class SessionEvictedListener
+		implements EntryEvictedListener<String, ExpiringSession> {
+
+	private ApplicationEventPublisher eventPublisher;
+
+	public SessionEvictedListener(ApplicationEventPublisher eventPublisher) {
+		this.eventPublisher = eventPublisher;
+	}
+
+	public void entryEvicted(EntryEvent<String, ExpiringSession> event) {
+		System.out.println("Session removed: " + event);
+		eventPublisher.publishEvent(new SessionExpiredEvent(this, event.getOldValue()));
+	}
+
+}
