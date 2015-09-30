a00d7fabfeb306ce7213f11ee106c090d616022e
Tommy Ludwig <thomas.ludwig@rakuten.com>
2015-09-30 22:38:08 +0900

    Add integration tests that look at the underlying Hazelcast Map used to store the Sessions.

---
+++ spring-session/src/integration-test/java/org/springframework/session/data/hazelcast/HazelcastRepositoryITests.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2002-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.session.data.hazelcast;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.data.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+/**
+ * Integration tests that check the underlying data source - in this case
+ * Hazelcast.
+ * 
+ * @author Tommy Ludwig
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class HazelcastRepositoryITests<S extends ExpiringSession> {
+
+	@Autowired
+	private HazelcastInstance hazelcast;
+	
+	@Autowired
+	private SessionRepository<S> repository;
+	
+	@Test
+	public void createAndDestorySession() {
+		S sessionToSave = repository.createSession();
+		String sessionId = sessionToSave.getId();
+		
+		IMap<String, S> hazelcastMap = hazelcast.getMap("spring:session:sessions");
+		
+		assertThat(hazelcastMap.size()).isEqualTo(0);
+		
+		repository.save(sessionToSave);
+		
+		assertThat(hazelcastMap.size()).isEqualTo(1);
+		assertThat(hazelcastMap.get(sessionId)).isEqualTo(sessionToSave);
+		
+		repository.delete(sessionId);
+		
+		assertThat(hazelcastMap.size()).isEqualTo(0);
+	}
+
+	@EnableHazelcastHttpSession
+	@Configuration
+	static class HazelcastSessionConfig {
+
+		@Bean
+		public HazelcastInstance embeddedHazelcast() {
+			Config hazelcastConfig = new Config();
+			return Hazelcast.newHazelcastInstance(hazelcastConfig);
+		}
+	}
+
+}
