7c53558454a160e0443d73366ecc9e0d32211c3f
Vedran Pavic <vedran.pavic@gmail.com>
2016-02-08 16:06:03 -0600

    Add integration tests for Hazelcast client use case
    Fixes gh-356

---
+++ spring-session/src/integration-test/java/org/springframework/session/hazelcast/AbstractHazelcastRepositoryITests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.session.hazelcast;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.SessionRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Abstract base class for Hazelcast integration tests.
+ * 
+ * @author Tommy Ludwig
+ * @author Vedran Pavic
+ */
+public abstract class AbstractHazelcastRepositoryITests<S extends ExpiringSession> {
+
+	@Autowired
+	private HazelcastInstance hazelcast;
+
+	@Autowired
+	private SessionRepository<S> repository;
+
+	@Test
+	public void createAndDestorySession() {
+		S sessionToSave = repository.createSession();
+		String sessionId = sessionToSave.getId();
+
+		IMap<String, S> hazelcastMap = hazelcast.getMap("spring:session:sessions");
+
+		assertThat(hazelcastMap.size()).isEqualTo(0);
+
+		repository.save(sessionToSave);
+
+		assertThat(hazelcastMap.size()).isEqualTo(1);
+		assertThat(hazelcastMap.get(sessionId)).isEqualTo(sessionToSave);
+
+		repository.delete(sessionId);
+
+		assertThat(hazelcastMap.size()).isEqualTo(0);
+	}
+
+}
---
+++ spring-session/src/integration-test/java/org/springframework/session/hazelcast/HazelcastServerRepositoryITests.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.session.hazelcast;
+
+import com.hazelcast.core.HazelcastInstance;
+import org.junit.runner.RunWith;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Integration tests that check the underlying data source - in this case
+ * Hazelcast Server.
+ *
+ * @author Tommy Ludwig
+ * @author Vedran Pavic
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class HazelcastServerRepositoryITests<S extends ExpiringSession>
+		extends AbstractHazelcastRepositoryITests<S> {
+
+	@EnableHazelcastHttpSession
+	@Configuration
+	static class HazelcastSessionConfig {
+
+		@Bean
+		public HazelcastInstance embeddedHazelcastServer() {
+			return HazelcastITestUtils.embeddedHazelcastServer();
+		}
+
+	}
+
+}
