c07583bd475a5b03431cb520353465e2d379b60d
Vedran Pavic <vedran.pavic@gmail.com>
2016-03-30 22:13:02 +0200

    Update WebSocket sample to use H2 console auto-configuration
    Fixes gh-457.

--- samples/websocket/src/main/java/sample/config/WebSecurityConfig.java
+++ samples/websocket/src/main/java/sample/config/WebSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -48,6 +49,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll();
+	}
+	// @formatter:on
+
+	// @formatter:off
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring().antMatchers("/h2-console/**");
 	}
 	// @formatter:on
 
