cce8dac4b72a826a981c73c15ee06278dc753e20
Rob Winch <rwinch@gopivotal.com>
2016-04-01 14:27:08 -0500

    Fix WebSocket AbstractMethodError
    Fixes gh-460

---
+++ samples/websocket/src/test/java/sample/ApplicationTests.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.sockjs.client.RestTemplateXhrTransport;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+/**
+ * @author Rob Winch
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest(randomPort = true)
+public class ApplicationTests {
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+
+	@Value("${local.server.port}")
+	String port;
+
+	@Autowired
+	WebSocketHandler webSocketHandler;
+
+	@Test
+	public void run() throws Exception {
+		List<Transport> transports = new ArrayList<Transport>(2);
+		transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+		transports.add(new RestTemplateXhrTransport());
+
+		SockJsClient sockJsClient = new SockJsClient(transports);
+		ListenableFuture<WebSocketSession> wsSession = sockJsClient.doHandshake(
+				this.webSocketHandler, "ws://localhost:" + this.port + "/sockjs");
+
+		this.thrown.expect(ExecutionException.class);
+		wsSession.get().sendMessage(new TextMessage("a"));
+	}
+}
