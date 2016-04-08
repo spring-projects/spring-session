2b4a349b1a8710b47e89e412d3f1ab511e828323
Vedran Pavic <vedran.pavic@gmail.com>
2016-04-08 19:38:14 +0200

    Use webjars for static resources in sample apps

--- samples/findbyusername/src/main/java/sample/config/SecurityConfig.java
+++ samples/findbyusername/src/main/java/sample/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
-				.antMatchers("/resources/**").permitAll().anyRequest().authenticated()
+				.antMatchers("/resources/**", "/webjars/**").permitAll().anyRequest().authenticated()
 				.and().logout().permitAll();
 	}
 	// end::config[]
