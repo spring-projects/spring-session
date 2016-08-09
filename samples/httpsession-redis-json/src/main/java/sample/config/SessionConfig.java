eb14698be5b88c51e5fb4546eb26799d4b9635a7
Jitendra Singh <jeet.mp3@gmail.com>
2016-08-09 23:56:08 +0530

    SecurityJacksonUtils class used to get security modules

--- samples/httpsession-redis-json/src/main/java/sample/config/SessionConfig.java
+++ samples/httpsession-redis-json/src/main/java/sample/config/SessionConfig.java
@@ -1,18 +1,12 @@
 package sample.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.security.jackson2.CoreJackson2SimpleModule;
-import org.springframework.security.web.jackson2.WebJackson2SimpleModule;
+import org.springframework.security.jackson2.SecurityJacksonModules;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY;
 
 /**
  * @author jitendra on 3/3/16.
@@ -37,18 +31,7 @@ public class SessionConfig {
      */
     ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setVisibility(
-                mapper.getVisibilityChecker()
-                        .withFieldVisibility(ANY)
-                        .withSetterVisibility(PUBLIC_ONLY)
-                        .withGetterVisibility(PUBLIC_ONLY)
-                        .withIsGetterVisibility(PUBLIC_ONLY)
-        );
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
-        mapper.registerModule(new CoreJackson2SimpleModule());
-        mapper.registerModule(new WebJackson2SimpleModule());
+        mapper.registerModules(SecurityJacksonModules.getModules());
         return mapper;
     }
 }
