89a3342ce03a2645e171e1a37be5ef8fcbeb07be
Eric Spiegelberg <eric@miletwentyfour.com>
2017-06-25 17:34:36 -0500

    Development of the findByIndexNameAndIndexValue() implementation. This code is raw and needs polish, tests, etc.

--- spring-session-neo4j/src/main/java/org/springframework/session/neo4j/OgmSessionRepository.java
+++ spring-session-neo4j/src/main/java/org/springframework/session/neo4j/OgmSessionRepository.java
@@ -81,7 +81,6 @@ public class OgmSessionRepository implements
 	
 	private static final String LIST_SESSIONS_BY_PRINCIPAL_NAME_QUERY = "match (n:%LABEL%) where n.principalName={principalName} return n order by n.creationTime desc";
 
-// TODO: Complete the deleteSessionByLastAccessTimeQuery and test
 	private static final String DELETE_SESSIONS_BY_LAST_ACCESS_TIME_QUERY = 
 			"match (n:%LABEL%) where n.maxInactiveInterval < n.maxInactiveInterval < ({lastAccessedTime} - n.lastAccessedTime) / 1000 detach delete n";
 
@@ -412,6 +411,7 @@ public class OgmSessionRepository implements
 		return sessionMap;
 	}
 
+	// TODO: Complete the deleteSessionByLastAccessTimeQuery and test
 	@Scheduled(cron = "${spring.session.cleanup.cron.expression:0 * * * * *}")
 	public void cleanUpExpiredSessions() {
 
