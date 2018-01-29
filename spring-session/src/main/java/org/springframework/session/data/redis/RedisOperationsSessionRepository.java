f2d1badd60687bab56c98a1474465d172b4771c9
Vedran Pavic <vedran.pavic@gmail.com>
2018-01-29 16:24:34 +0100

    Fix misc typos
    Closes gh-985

--- spring-session/src/main/java/org/springframework/session/data/redis/RedisOperationsSessionRepository.java
+++ spring-session/src/main/java/org/springframework/session/data/redis/RedisOperationsSessionRepository.java
@@ -277,7 +277,7 @@ public class RedisOperationsSessionRepository implements
 	static final String LAST_ACCESSED_ATTR = "lastAccessedTime";
 
 	/**
-	 * The prefix of the key for used for session attributes. The suffix is the name of
+	 * The prefix of the key used for session attributes. The suffix is the name of
 	 * the session attribute. For example, if the session contained an attribute named
 	 * attributeName, then there would be an entry in the hash named
 	 * sessionAttr:attributeName that mapped to its value.
