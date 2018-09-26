3940a22d5e0bbe68a08ae02c93eb95454cd67811
Vedran Pavic <vedran.pavic@gmail.com>
2018-09-26 12:36:46 +0200

    Disable network join in Hazelcast samples

--- samples/hazelcast/src/main/java/sample/Initializer.java
+++ samples/hazelcast/src/main/java/sample/Initializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 the original author or authors.
+ * Copyright 2014-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,7 @@ public class Initializer implements ServletContextListener {
 		Config cfg = new Config();
 		NetworkConfig netConfig = new NetworkConfig();
 		netConfig.setPort(getAvailablePort());
+		netConfig.getJoin().getMulticastConfig().setEnabled(false);
 		cfg.setNetworkConfig(netConfig);
 		SerializerConfig serializer = new SerializerConfig().setTypeClass(Object.class)
 				.setImplementation(new ObjectStreamSerializer());
