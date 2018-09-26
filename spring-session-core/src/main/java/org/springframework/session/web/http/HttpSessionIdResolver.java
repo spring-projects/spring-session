4c5f22900d2bdb9ba4b07f5259eea310428a9c21
Vedran Pavic <vedran.pavic@gmail.com>
2018-09-26 13:21:44 +0200

    Polish

--- spring-session-core/src/main/java/org/springframework/session/web/http/HttpSessionIdResolver.java
+++ spring-session-core/src/main/java/org/springframework/session/web/http/HttpSessionIdResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 the original author or authors.
+ * Copyright 2014-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Rob Winch
  * @author Vedran Pavic
- * @since 1.0
+ * @since 2.0.0
  */
 public interface HttpSessionIdResolver {
 
