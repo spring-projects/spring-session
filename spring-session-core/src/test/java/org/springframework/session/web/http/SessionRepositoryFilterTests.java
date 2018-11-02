c8f3d1a1ec829347100d89df3247806d3dc5b83f
Josh Cummings <josh.cummings@gmail.com>
2018-11-02 21:18:53 +0100

    Commit Session on Include Dispatch
    The servlet spec disallows any writing of headers after an include has been issued.
    
    This commit intercepts the include and commits the session, then
    allowing the include to proceed.
    
    See: #1243

--- spring-session-core/src/test/java/org/springframework/session/web/http/SessionRepositoryFilterTests.java
+++ spring-session-core/src/test/java/org/springframework/session/web/http/SessionRepositoryFilterTests.java
@@ -1170,6 +1170,21 @@ public class SessionRepositoryFilterTests {
 		});
 	}
 
+	@Test
+	public void doFilterInclude() throws Exception {
+		doFilter(new DoInFilter() {
+			@Override
+			public void doFilter(HttpServletRequest wrappedRequest,
+					HttpServletResponse wrappedResponse) throws IOException, ServletException {
+				String id = wrappedRequest.getSession().getId();
+				wrappedRequest.getRequestDispatcher("/").include(wrappedRequest, wrappedResponse);
+				assertThat(
+						SessionRepositoryFilterTests.this.sessionRepository.findById(id))
+						.isNotNull();
+			}
+		});
+	}
+
 	// --- HttpSessionIdResolver
 
 	@Test
