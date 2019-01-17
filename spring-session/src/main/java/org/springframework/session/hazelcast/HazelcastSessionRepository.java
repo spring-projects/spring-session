298b73fc95f3b78682207045ebb261e50ce1786d
Joachim Todt <joachim.todt@lotto24.de>
2019-01-17 16:47:45 +0100

    Revert "use IMap#delete that doesn't deserialize removed value to improve performance"
    This reverts commit c61ea5f

--- spring-session/src/main/java/org/springframework/session/hazelcast/HazelcastSessionRepository.java
+++ spring-session/src/main/java/org/springframework/session/hazelcast/HazelcastSessionRepository.java
@@ -232,7 +232,7 @@ public class HazelcastSessionRepository implements
 	}
 
 	public void delete(String id) {
-		this.sessions.delete(id);
+		this.sessions.remove(id);
 	}
 
 	public Map<String, HazelcastSession> findByIndexNameAndIndexValue(String indexName,
