4a2658a40daf27d4f818b2c44f49ea713ab04a85
Kevin <kevin.k.a.cheung@accenture.com>
2019-03-29 10:19:28 +0800

    Issue #1395: Fix travis build issue

--- spring-session/src/main/java/org/springframework/session/data/redis/RedisSessionExpirationPolicy.java
+++ spring-session/src/main/java/org/springframework/session/data/redis/RedisSessionExpirationPolicy.java
@@ -132,12 +132,10 @@ final class RedisSessionExpirationPolicy {
 		this.redis.delete(expirationKey);
 		for (Object session : sessionsToExpire) {
 			String sessionKey = getSessionKey((String) session);
-			
-			String[] expiresSessionKey = sessionKey.split(":");
-			if (expiresSessionKey.length > 0) {
-				this.redisSession.delete(expiresSessionKey[expiresSessionKey.length-1]);
-			}
-			
+
+			String sessionId = sessionKey.substring(sessionKey.lastIndexOf(":") + 1);
+			this.redisSession.delete(sessionId);
+
 			touch(sessionKey);
 		}
 	}
