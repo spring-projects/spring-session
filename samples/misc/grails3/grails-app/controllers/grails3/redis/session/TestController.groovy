2f6c7d22df7d97f47c2018161d8e8abf8637e9c6
Vedran Pavic <vedran.pavic@gmail.com>
2019-06-04 23:08:08 +0200

    Simplify project structure
    - harmonize module and directory names
    - optimize Gradle settings
    - remove unused Grails sample
    
    Resolves: #1401

--- samples/misc/grails3/grails-app/controllers/grails3/redis/session/TestController.groovy
+++
@@ -1,8 +0,0 @@
-package grails3.redis.session
-
-import grails.plugin.springsecurity.annotation.Secured
-
-class TestController {
-  @Secured('ROLE_ADMIN')
-  def index() { } // Renders `test/index.gsp`
-}
--- samples/misc/grails3/grails-app/views/index.gsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-  <title>Index</title>
-</head>
-<body>
-  Left blank, goto <a href="/test">test</a>
-</body>
-</html>
