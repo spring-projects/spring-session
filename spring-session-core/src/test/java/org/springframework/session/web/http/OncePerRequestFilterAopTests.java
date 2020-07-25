190a9915554fcfafa4268774e9b77851b0204421
bboyz269 <bboyz269@yahoo.com.vn>
2020-07-25 07:38:10 +0700

    Closes gh-1658
    fix typos

--- spring-session-core/src/test/java/org/springframework/session/web/http/OncePerRequestFilterAopTests.java
+++ spring-session-core/src/test/java/org/springframework/session/web/http/OncePerRequestFilterAopTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ class OncePerRequestFilterAopTests {
 		}
 
 		@AfterReturning("execution(* SessionRepositoryFilter.doFilterInternal(..))")
-		public void doInternalFilterPoincut() {
+		public void doInternalFilterPointcut() {
 			// no op
 		}
 
