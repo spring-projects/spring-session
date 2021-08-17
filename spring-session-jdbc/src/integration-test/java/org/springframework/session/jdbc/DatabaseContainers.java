9fde87c11b001b665e74a852985f90458078d399
Eleftheria Stein <estein@vmware.com>
2021-08-17 13:23:08 +0200

    Upgrade test dependencies

--- spring-session-jdbc/src/integration-test/java/org/springframework/session/jdbc/DatabaseContainers.java
+++ spring-session-jdbc/src/integration-test/java/org/springframework/session/jdbc/DatabaseContainers.java
@@ -42,11 +42,11 @@ final class DatabaseContainers {
 	}
 
 	static MariaDBContainer<?> mariaDb() {
-		return new MariaDBContainer<>("mariadb:10.6.0");
+		return new MariaDBContainer<>("mariadb:10.6.4");
 	}
 
 	static MySQLContainer<?> mySql() {
-		return new MySQLContainer<>("mysql:8.0.25");
+		return new MySQLContainer<>("mysql:8.0.26");
 	}
 
 	static OracleContainer oracle() {
@@ -68,7 +68,7 @@ final class DatabaseContainers {
 	}
 
 	static PostgreSQLContainer<?> postgreSql() {
-		return new PostgreSQLContainer<>("postgres:13.3");
+		return new PostgreSQLContainer<>("postgres:13.4");
 	}
 
 	static MSSQLServerContainer<?> sqlServer() {
