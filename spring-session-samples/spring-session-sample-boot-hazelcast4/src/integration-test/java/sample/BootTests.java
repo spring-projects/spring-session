79fbca24ebbcb8f9938ba99465d59fe71a4691ff
Vedran Pavic <vedran.pavic@gmail.com>
2021-08-27 01:36:07 +0200

    Make Hazelcast session repository bean factory return type more specific
    The declared return type of Hazelcast session repository bean factory method (i.e. HazelcastHttpSessionConfiguration#sessionRepository) was changed to SessionRepository<?> when support for Hazelcast 4 was added. This breaks Spring Boot's ability to auto-configure sessions endpoint, which is @ConditionalOnBean(FindByIndexNameSessionRepository.class), as the current return type is not specific enough to satisfy this condition.
    
    This commit changes the return type of Hazelcast session repository bean factory method to FindByIndexNameSessionRepository<?>.
    
    Closes: gh-1905

--- spring-session-samples/spring-session-sample-boot-hazelcast4/src/integration-test/java/sample/BootTests.java
+++ spring-session-samples/spring-session-sample-boot-hazelcast4/src/integration-test/java/sample/BootTests.java
@@ -24,11 +24,16 @@ import sample.pages.HomePage;
 import sample.pages.LoginPage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.session.SessionsEndpoint;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.session.hazelcast.Hazelcast4IndexedSessionRepository;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -47,6 +52,12 @@ class BootTests {
 	@AfterEach
 	void tearDown() {
 		this.driver.quit();
+	}
+
+	@Test // gh-1905
+	void contextLoads(ApplicationContext context) {
+		assertThat(context.getBeansOfType(Hazelcast4IndexedSessionRepository.class)).hasSize(1);
+		assertThat(context.getBeansOfType(SessionsEndpoint.class)).hasSize(1);
 	}
 
 	@Test
