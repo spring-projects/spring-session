e7d58f6b0338a160443fcd1579b9589cf4a2a48a
Eleftheria Stein <estein@vmware.com>
2021-09-08 12:04:26 +0200

    Increase session timeout in Hazelcast tests
    It's possible that the session is expiring before the assertions can be made in the tests, causing them to fail.
    
    Issue gh-1912

--- spring-session-hazelcast/src/integration-test/java/org/springframework/session/hazelcast/SessionEventHazelcastIndexedSessionRepositoryTests.java
+++ spring-session-hazelcast/src/integration-test/java/org/springframework/session/hazelcast/SessionEventHazelcastIndexedSessionRepositoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @WebAppConfiguration
 class SessionEventHazelcastIndexedSessionRepositoryTests<S extends Session> {
 
-	private static final int MAX_INACTIVE_INTERVAL_IN_SECONDS = 1;
+	private static final int MAX_INACTIVE_INTERVAL_IN_SECONDS = 2;
 
 	@Autowired
 	private SessionRepository<S> repository;
