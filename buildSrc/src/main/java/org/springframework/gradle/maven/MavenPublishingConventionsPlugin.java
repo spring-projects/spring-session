2110ef0c2d060998147cdd270fcd56c01299ce29
Eleftheria Stein <estein@vmware.com>
2021-10-29 15:40:06 +0200

    Fix scm attributes in maven pom.xml
    Closes gh-1897

--- buildSrc/src/main/java/org/springframework/gradle/maven/MavenPublishingConventionsPlugin.java
+++ buildSrc/src/main/java/org/springframework/gradle/maven/MavenPublishingConventionsPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.springframework.gradle.maven;
 
 import org.gradle.api.Action;
@@ -30,7 +46,7 @@ public class MavenPublishingConventionsPlugin implements Plugin<Project> {
 	}
 
 	private void customizePom(MavenPom pom, Project project) {
-		pom.getUrl().set("https://spring.io/projects/spring-security");
+		pom.getUrl().set("https://spring.io/projects/spring-session");
 		pom.getName().set(project.provider(project::getName));
 		pom.getDescription().set(project.provider(project::getDescription));
 		pom.organization(this::customizeOrganization);
@@ -62,14 +78,14 @@ public class MavenPublishingConventionsPlugin implements Plugin<Project> {
 	}
 
 	private void customizeScm(MavenPomScm scm) {
-		scm.getConnection().set("scm:git:git://github.com/spring-projects/spring-security.git");
-		scm.getDeveloperConnection().set("scm:git:ssh://git@github.com/spring-projects/spring-security.git");
-		scm.getUrl().set("https://github.com/spring-projects/spring-security");
+		scm.getConnection().set("scm:git:git://github.com/spring-projects/spring-session.git");
+		scm.getDeveloperConnection().set("scm:git:ssh://git@github.com/spring-projects/spring-session.git");
+		scm.getUrl().set("https://github.com/spring-projects/spring-session");
 	}
 
 	private void customizeIssueManagement(MavenPomIssueManagement issueManagement) {
 		issueManagement.getSystem().set("GitHub");
-		issueManagement.getUrl().set("https://github.com/spring-projects/spring-security/issues");
+		issueManagement.getUrl().set("https://github.com/spring-projects/spring-session/issues");
 	}
 
 	private void customizeJavaPlugin(Project project) {
