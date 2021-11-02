5fdbfce4c2459eccaf9d145f1e9be528a883bd85
Eleftheria Stein <estein@vmware.com>
2021-11-02 14:28:31 +0100

    Update Gradle to 7.2
    Closes gh-1944

--- buildSrc/src/main/groovy/io/spring/gradle/convention/TestsConfigurationPlugin.groovy
+++ buildSrc/src/main/groovy/io/spring/gradle/convention/TestsConfigurationPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -24,7 +24,7 @@ import org.gradle.jvm.tasks.Jar
  * Adds the ability to depends on the test jar within other projects using:
  *
  * <code>
- * testCompile project(path: ':foo', configuration: 'tests')
+ * testImplementation project(path: ':foo', configuration: 'tests')
  * </code>
  *
  * @author Rob Winch
