ab5c7278465182f78cbc4bf9da3638f53086aad9
Eleftheria Stein <estein@vmware.com>
2022-01-27 14:11:26 +0100

    Update Websocket sample to be compatible with H2 2.0
    Closes gh-2013

--- spring-session-samples/spring-session-sample-boot-websocket/src/main/java/sample/data/User.java
+++ spring-session-samples/spring-session-sample-boot-websocket/src/main/java/sample/data/User.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  *
  * @author Rob Winch
  */
-@Entity
+@Entity(name = "custom_user")
 public class User implements Serializable {
 
 	@Id
