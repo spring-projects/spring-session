4b34f35b829be019e28105424f890c2425a13f90
Vedran Pavic <vedran@vedranpavic.com>
2022-09-23 08:57:04 +0200

    Replace JSR-250 annotations with standard Spring lifecycle callbacks

--- spring-session-hazelcast/src/test/java/org/springframework/session/hazelcast/HazelcastIndexedSessionRepositoryTests.java
+++ spring-session-hazelcast/src/test/java/org/springframework/session/hazelcast/HazelcastIndexedSessionRepositoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ class HazelcastIndexedSessionRepositoryTests {
 	void setUp() {
 		given(this.hazelcastInstance.<String, MapSession>getMap(anyString())).willReturn(this.sessions);
 		this.repository = new HazelcastIndexedSessionRepository(this.hazelcastInstance);
-		this.repository.init();
+		this.repository.afterPropertiesSet();
 	}
 
 	@Test
