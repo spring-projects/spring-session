c183934aa2136ad453f81941e5de61b406e4fce0
Rob Winch <rwinch@users.noreply.github.com>
2022-11-03 13:31:18 -0500

    Initial

---
+++ .github/actions/publish-docs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HOST="$1"
+HOST_PATH="$2"
+SSH_PRIVATE_KEY="$3"
+SSH_KNOWN_HOST="$4"
+SSH_PRIVATE_KEY_PATH="$HOME/.ssh/${GITHUB_REPOSITORY:-publish-docs}"
+
+if [ "$#" -ne 4 ]; then
+  echo -e "not enough arguments USAGE:\n\n$0 \$HOST \$HOST_PATH \$SSH_PRIVATE_KEY \$SSH_KNOWN_HOST\n\n" >&2
+  exit 1
+fi
+
+(
+  set -e
+  install -m 600 -D /dev/null "$SSH_PRIVATE_KEY_PATH"
+  echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_KEY_PATH"
+  echo "$SSH_KNOWN_HOST" > ~/.ssh/known_hosts
+  rsync --delete -avze "ssh -i $SSH_PRIVATE_KEY_PATH" build/site/ "$HOST:$HOST_PATH"
+)
+exit_code=$?
+
+rm -f "$SSH_PRIVATE_KEY_PATH"
+
+exit $exit_code
