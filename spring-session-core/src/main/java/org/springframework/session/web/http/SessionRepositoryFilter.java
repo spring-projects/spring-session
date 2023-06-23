dbd9567e2342624eb1f2ee696e50c926c1e486bc
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-06-23 15:05:54 -0300

    Fix import formatting
    Issue gh-2285

--- spring-session-core/src/main/java/org/springframework/session/web/http/SessionRepositoryFilter.java
+++ spring-session-core/src/main/java/org/springframework/session/web/http/SessionRepositoryFilter.java
@@ -30,6 +30,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
