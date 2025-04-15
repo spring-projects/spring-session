ccbb0b5cb868b610b171956eeb3c6f4062664d8b
Yanming Zhou <zhouyanming@gmail.com>
2025-04-15 10:55:26 +0800

    Polish usage of spring-boot-testcontainers
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-session-samples/spring-session-sample-boot-jdbc-json-attribute/src/test/java/sample/TestContainersConfig.java
+++ spring-session-samples/spring-session-sample-boot-jdbc-json-attribute/src/test/java/sample/TestContainersConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package sample;
 
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,17 +26,10 @@ import org.springframework.context.annotation.Bean;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfig {
 
-	static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1"))
-		.withExposedPorts(5432);
-
-	static {
-		postgresql.start();
-	}
-
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return postgresql;
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1"));
 	}
 
 }
