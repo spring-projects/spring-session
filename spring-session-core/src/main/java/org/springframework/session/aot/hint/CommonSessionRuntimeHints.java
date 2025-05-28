19493415c1538402294925ec9f2499660b92fc48
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-05-28 12:23:09 +0200

    Add Common Serialization Hints

--- spring-session-core/src/main/java/org/springframework/session/aot/hint/CommonSessionRuntimeHints.java
+++ spring-session-core/src/main/java/org/springframework/session/aot/hint/CommonSessionRuntimeHints.java
@@ -16,13 +16,21 @@
 
 package org.springframework.session.aot.hint;
 
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.TreeSet;
 
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.session.MapSession;
 
 /**
  * A {@link RuntimeHintsRegistrar} for common session hints.
@@ -33,16 +41,19 @@ class CommonSessionRuntimeHints implements RuntimeHintsRegistrar {
 
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-		Arrays
-			.asList(TypeReference.of(String.class), TypeReference.of(ArrayList.class), TypeReference.of(TreeSet.class),
-					TypeReference.of(Number.class), TypeReference.of(Long.class), TypeReference.of(Integer.class),
-					TypeReference.of(StackTraceElement.class), TypeReference.of(Throwable.class),
-					TypeReference.of(Exception.class), TypeReference.of(RuntimeException.class),
-					TypeReference.of("java.util.Collections$UnmodifiableCollection"),
-					TypeReference.of("java.util.Collections$UnmodifiableList"),
-					TypeReference.of("java.util.Collections$EmptyList"),
-					TypeReference.of("java.util.Collections$UnmodifiableRandomAccessList"),
-					TypeReference.of("java.util.Collections$UnmodifiableSet"))
+		Arrays.asList(TypeReference.of(String.class), TypeReference.of(Number.class), TypeReference.of(Long.class),
+				TypeReference.of(Integer.class), TypeReference.of(URL.class), TypeReference.of(Instant.class),
+				TypeReference.of(Duration.class), TypeReference.of("java.time.Ser"),
+				TypeReference.of(StackTraceElement.class), TypeReference.of(Throwable.class),
+				TypeReference.of(Exception.class), TypeReference.of(RuntimeException.class),
+				TypeReference.of(ArrayList.class), TypeReference.of(TreeSet.class), TypeReference.of(HashMap.class),
+				TypeReference.of(LinkedHashMap.class), TypeReference.of(HashSet.class),
+				TypeReference.of(LinkedHashSet.class), TypeReference.of("java.util.Collections$UnmodifiableCollection"),
+				TypeReference.of("java.util.Collections$UnmodifiableList"),
+				TypeReference.of("java.util.Collections$EmptyList"),
+				TypeReference.of("java.util.Collections$UnmodifiableRandomAccessList"),
+				TypeReference.of("java.util.Collections$UnmodifiableSet"),
+				TypeReference.of("java.util.Collections$UnmodifiableMap"), TypeReference.of(MapSession.class))
 			.forEach(hints.serialization()::registerType);
 	}
 
